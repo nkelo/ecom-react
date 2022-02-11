5be3eabd016a6b55af99192a4aa801e017b7d05b
Samu <samu@wsb.co.za>
2022-02-11 17:35:38 +0200

    commit

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Rating(value, text, color) {
+function Rating({ value, text, color }) {
   return (
     <div className="rating">
       <span>
@@ -9,15 +9,74 @@ function Rating(value, text, color) {
           className={
             value >= 1
               ? "fas fa-star"
-              : value >= 0.6
+              : value >= 0.5
               ? "fas fa-star-half-alt"
-              : "fas fa-star"
+              : "far fa-star"
           }
         >
           {" "}
         </i>
       </span>
-      {color}dsds
+
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 2
+              ? "fas fa-star"
+              : value >= 1.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        >
+          {" "}
+        </i>
+      </span>
+
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 3
+              ? "fas fa-star"
+              : value >= 2.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        >
+          {" "}
+        </i>
+      </span>
+
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 4
+              ? "fas fa-star"
+              : value >= 3.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        >
+          {" "}
+        </i>
+      </span>
+
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 5
+              ? "fas fa-star"
+              : value >= 4.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+
+      <span>{text && text}</span>
     </div>
   );
 }
